a290d66e229cbb39897218c0e656163a0933ce44
Amey Dhamgunde <ameydhamgunde@Ameys-MacBook-Air.local>
2018-10-13 02:56:54 -0400

    Adding Features

--- js/ip.js
+++ js/ip.js
@@ -4,6 +4,15 @@ function useIP() {
         console.log(data);
         // TODO: use data.city for later
         usingGeolocation || moveToUser(data.latitude, data.longitude);
+        getWatch(data.city);
       }
   );
+}
+
+function getWatch(city) {
+  $.getJSON("https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name="+city+"&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg", 
+    function(cityData) {
+      console.log(cityData);
+    }
+  )
 }
