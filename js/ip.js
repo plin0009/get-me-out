26f97c03865bb329576820ecd71a755bbf5d2dbd
plin0009 <plin0009@gmail.com>
2018-10-13 02:26:29 -0400

    Allowed instant map positioning based on IP address.

--- js/ip.js
+++ js/ip.js
@@ -1,5 +1,7 @@
 $.getJSON("https://json.geoiplookup.io/?callback=?",
     function (data) {
       console.log(data);
+      // TODO: use data.city for later
+      usingGeolocation || moveToUser(data.latitude, data.longitude);
     }
 );
