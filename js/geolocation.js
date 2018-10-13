a4173c838496a053613be6d03d3cadc9bd2f1c52
plin0009 <plin0009@gmail.com>
2018-10-13 00:41:43 -0400

    Used Geolocation API to obtain the user's location.

---
+++ js/geolocation.js
@@ -0,0 +1,17 @@
+function useGeolocation() {
+  if ("geolocation" in navigator) {
+    // use success and error callback functions
+    navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError);
+  } else {
+    // browser does not support geolocation api
+    console.log("Navigator object not available");
+  }
+}
+function geolocationSuccess(position) {
+  // got the geolocation
+  console.log("User geolocation: ", position.coords.latitude, position.coords.longitude);
+}
+function geolocationError(error) {
+  // most likely user denied to use location
+  console.log("Could not get location (" + error.code + ")", error.message);
+}
