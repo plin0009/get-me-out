f9bc4609f5db8df931de282844a9bf88afc8e80e
plin0009 <plin0009@gmail.com>
2018-10-13 12:56:20 -0400

    Started using firebaseui.

---
+++ js/firebaseui.js
@@ -0,0 +1,13 @@
+var uiConfig = {
+  signInSuccessUrl: alert("123"),
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+    firebase.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+  tosUrl: "tos.html",
+  privacyPolicyUrl: "pp.html"
+};
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start("#firebase-ui-container", uiConfig);
