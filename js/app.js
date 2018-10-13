277767e0d93a445552b5d4702d19da142e058062
Alan Wang <alan.wang9018@gmail.com>
2018-10-13 12:24:15 -0400

    worked on the buttons

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@
 //});
 $(function(){
     $(window).scroll(function() {
+        if ($(this).width() < 992) {
+            if ($(this).height() <= 768) {
                 if($(this).scrollTop() > 1000) {
                     $('#map').animate({'opacity': 'show', 'paddingTop': 0}, 2000);
                 }
