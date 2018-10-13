f500e3c90a15003fa2b9d0e7685edef6805f60ba
Amey Dhamgunde <ameydhamgunde@Ameys-MacBook-Air.local>
2018-10-13 03:01:29 -0400

    testing

--- js/ip.js
+++ js/ip.js
@@ -10,7 +10,7 @@ function useIP() {
 }
 
 function getWatch(city) {
-  $.getJSON("https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name="+city+"&app_id=DemoAppId01082013GAL&app_code=AJKnXv84fjrb0KIHawS0Tg", 
+  $.getJSON("https://weather.cit.api.here.com/weather/1.0/report.json?product=alerts&name="+city+"&app_id=MNMVa60AhFwhk7UGo1lD&app_code=xY8cMEGImPL_5mndE7WeWQ", 
     function(cityData) {
       console.log(cityData);
     }
