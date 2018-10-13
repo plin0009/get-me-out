66fdb737da455bc719bd70d896539e928f9b6a6a
Alan Wang <alan.wang9018@gmail.com>
2018-10-13 12:28:48 -0400

    scrolling animations

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@
    //$("#About").slideUp(1000);
 //});
 $(function(){
-<<<<<<< HEAD
     $(window).scroll(function() {
         if ($(this).width() < 992) {
             if ($(this).height() <= 768) {
@@ -10,11 +9,6 @@ $(function(){
                     $('#map').animate({'opacity': 'show', 'paddingTop': 0}, 2000);
                 }
             }
-=======
-  $(window).scroll(function() {
-        if($(this).scrollTop() > 1000) {
-            $('#map').animate({'opacity': 'show', 'paddingTop': 0}, 2000);
->>>>>>> 866c9e541d813caf5cffb13bb74e5cf4c03e4240
         }
       })
     }
