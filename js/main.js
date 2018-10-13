a0293e1e4d57582e53617c9bddff0b785170ac27
plin0009 <plin0009@gmail.com>
2018-10-13 14:28:56 -0400

    Moved scrolling to main.js.

--- js/main.js
+++ js/main.js
@@ -2,5 +2,14 @@ window.onload = function() {
   // everything that happens on pageload goes here
   loadMap();
   useIP();
+  $(window).scroll(function() {
+    if ($(this).width() < 992) {
+        if ($(this).height() <= 768) {
+            if($(this).scrollTop() > 1000) {
+                $('#map').animate({'opacity': 'show', 'paddingTop': 0}, 2000);
+            }
+        }
+    }
+  });
   //useGeolocation();
 }
