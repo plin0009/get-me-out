1c0e9d45f1b87e2dfbee6f3a1bf5b0ce947115b7
Amey Dhamgunde <ameydhamgunde@Ameys-MacBook-Air.local>
2018-10-14 10:46:24 -0400

    testing

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
-var map, group = new H.map.Group(), platform, targetlat, targetlng, maneuversGroup;
+var map, group = new H.map.Group(), platform, targetlat, targetlng;
+var maneuversGroup = new  H.map.Group();
 
 function loadMap() {
     platform = new H.service.Platform({
@@ -24,7 +25,6 @@ function loadMap() {
   var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
   var ui = H.ui.UI.createDefault(map, defaultLayers);
   map.addObject(group);
-  map.addObject(maneuversGroup);
 }
 
 function moveToUser(latitude, longitude) {
@@ -138,7 +138,7 @@ function addRouteShapeToMap (route) {
 function addManueversToMap (route) {
     var svgMarkup = '<svg width="20" height="20" xmlns="http://www.w3.org/2000/svg"> <circle cx="8" cy="8" r="8" fill="rgba(26, 24, 41, 0.7)" stroke="white" stroke-width="1"/></svg>';
     var dot = new H.map.Icon(svgMarkup, {anchor: {x:8, y:8}});
-    maneuversGroup = new  H.map.Group();
+    
 
     for (i = 0;  i < route.leg.length; i += 1) {
         for (j = 0;  j < route.leg[i].maneuver.length; j += 1) {
@@ -154,7 +154,7 @@ function addManueversToMap (route) {
         }
     }
 
-    
+    map.addObject(maneuversGroup);
 }
 
 
