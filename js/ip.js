cba82fce5e1a36efc9ea65e1fed2f83effe2e4c7
plin0009 <plin0009@gmail.com>
2018-10-14 11:52:14 -0400

    Created local test file.

--- js/ip.js
+++ js/ip.js
@@ -1,10 +1,11 @@
+var testData = true;
 function useIP() {
   $.getJSON("https://json.geoiplookup.io/?callback=?",
       function (data) {
         //calculateRouteFromAtoB(data.latitude, data.longitude, 42.3526257, -83.2392885);
         moveToUser(data.latitude, data.longitude);
         saveLocation(data.latitude, data.longitude);
-        getWatch(data.latitude, data.longitude);
+        testData && loadTest() || getWatch(data.latitude, data.longitude);
 
       }
   );
@@ -24,35 +25,42 @@ function getWatch(lat, long) {
       app_id: 'devportal-demo-20180625',
       app_code: '9v2BkviRwi9Ot26kp2IysQ',
     },
-    success: function (watchData) {
-      var contains = false;
-      if (watchData.nwsAlerts && watchData.nwsAlerts.watch) {
-        contains = true;
-        for (i = watchData.nwsAlerts.watch.length-1; i >= 0; i--) {
-          if (watchData.nwsAlerts.watch[i].type == 9) {
-            addInfoBlock(1, watchData.nwsAlerts.watch[i].description, watchData.nwsAlerts.watch[i].message, watchData.nwsAlerts.watch[i].validFromTimeLocal, watchData.nwsAlerts.watch[i].severity);
-            for (j = 0; j < watchData.nwsAlerts.watch[watchData.nwsAlerts.watch.length-1].zone.length; j++) {
-              plotStaticPointWatch(watchData.nwsAlerts.watch[watchData.nwsAlerts.watch.length-1].zone[j].latitude, watchData.nwsAlerts.watch[watchData.nwsAlerts.watch.length-1].zone[j].longitude);
-            }
+    success: loadThings
+  });
+}
+
+function loadTest() {
+  fetch("TestData.json").then(response => response.json()).then(json=>loadThings(json));
+}
+
+function loadThings (watchData) {
+    var contains = false;
+    if (watchData.nwsAlerts && watchData.nwsAlerts.watch) {
+      contains = true;
+      for (i = watchData.nwsAlerts.watch.length-1; i >= 0; i--) {
+        if (watchData.nwsAlerts.watch[i].type == 9) {
+          addInfoBlock(1, watchData.nwsAlerts.watch[i].description, watchData.nwsAlerts.watch[i].message, watchData.nwsAlerts.watch[i].validFromTimeLocal, watchData.nwsAlerts.watch[i].severity);
+          for (j = 0; j < watchData.nwsAlerts.watch[watchData.nwsAlerts.watch.length-1].zone.length; j++) {
+            plotStaticPointWatch(watchData.nwsAlerts.watch[watchData.nwsAlerts.watch.length-1].zone[j].latitude, watchData.nwsAlerts.watch[watchData.nwsAlerts.watch.length-1].zone[j].longitude);
           }
         }
-      }
-      if (watchData.nwsAlerts && watchData.nwsAlerts.warning) {
-        contains = true;
-        for (i = watchData.nwsAlerts.warning.length-1; i >= 0; i--) {
-          if (watchData.nwsAlerts.warning[i].type == 9) {
-            addInfoBlock(0, watchData.nwsAlerts.warning[i].description, watchData.nwsAlerts.warning[i].message, watchData.nwsAlerts.warning[i].validFromTimeLocal, watchData.nwsAlerts.warning[i].severity);
-            for (j = 0; j < watchData.nwsAlerts.warning[watchData.nwsAlerts.warning.length-1].zone.length; j++) {
-              plotStaticPointWarning(watchData.nwsAlerts.warning[watchData.nwsAlerts.warning.length-1].zone[j].latitude, watchData.nwsAlerts.warning[watchData.nwsAlerts.warning.length-1].zone[j].longitude);
-            }
-          }
-        }
-      }
-      if (contains) {
-        ending();
-      } else {
-        moveToUser();
       }
     }
-  });
+    if (watchData.nwsAlerts && watchData.nwsAlerts.warning) {
+      contains = true;
+      for (i = watchData.nwsAlerts.warning.length-1; i >= 0; i--) {
+        if (watchData.nwsAlerts.warning[i].type == 9) {
+          addInfoBlock(0, watchData.nwsAlerts.warning[i].description, watchData.nwsAlerts.warning[i].message, watchData.nwsAlerts.warning[i].validFromTimeLocal, watchData.nwsAlerts.warning[i].severity);
+          for (j = 0; j < watchData.nwsAlerts.warning[watchData.nwsAlerts.warning.length-1].zone.length; j++) {
+            plotStaticPointWarning(watchData.nwsAlerts.warning[watchData.nwsAlerts.warning.length-1].zone[j].latitude, watchData.nwsAlerts.warning[watchData.nwsAlerts.warning.length-1].zone[j].longitude);
+          }
+        }
+      }
+    }
+    if (contains) {
+      ending();
+    } else {
+      moveToUser();
+    }
+  }
 }
