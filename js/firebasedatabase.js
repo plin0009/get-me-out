ccbd0a467fc41da6063c6d4c76d6f9faeb825d1f
plin0009 <plin0009@gmail.com>
2018-10-13 21:08:07 -0400

    Added dates to the database.

--- js/firebasedatabase.js
+++ js/firebasedatabase.js
@@ -12,16 +12,18 @@ function saveLocation(lat, long) {
     userRef.set({
       fullName: name,
       location: [latitude, longitude],
-      active: new Date().toString()
+      active: new Date().getUTCDate()
     });
   } else {
     console.log("Not ready to save to database.");
   }
 }
-function getNames() {
+function getUsers() {
   database.ref("users").once("value", function (snapshot) {
     let user = snapshot.val();
-    others(user.location[0], user.location[1], user.fullName.split(" ").map(x => x.substr(0,1)).join(""));
+    if (user.uid != uid && user.active + 100000 < new Date().getUTCDate()) {
+      others(user.location[0], user.location[1], user.fullName.split(" ").map(x => x.substr(0,1)).join(""));
+    }
   }, function (err) {
     console.log(err);
   })
