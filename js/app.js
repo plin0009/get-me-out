55b5df921affe00d2b0c2c3cc01a38344e88c924
Alan Wang <alan.wang9018@gmail.com>
2018-10-13 22:55:12 -0400

    initialized fullpage

--- js/app.js
+++ js/app.js
@@ -1,19 +1,28 @@
 $(document).ready(function(){
   $("#mapButton").click(function(){
     $('html, body').animate({
-      scrollTop: $("#map").offset().top-70
+      scrollTop: $("#map").offset().top-40
    }, 500);
   });
   $("#aboutButton").click(function(){
     $('html, body').animate({
-      scrollTop: $("#about").offset().top-70
+      scrollTop: $("#about").offset().top-40
    }, 500);
   });
   $("#startButton").click(function(){
     $('html, body').animate({
-      scrollTop: $("#start").offset().top-70
+      scrollTop: $("#start").offset().top-1000
    }, 500);
   });
+
+  $('#fullpage').fullpage({
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true
+	});
+
+	//methods
+	$.fn.fullpage.setAllowScrolling(false);
 });
 
 
